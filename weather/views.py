81af621c5f8cfa9f5e48cc61417b7456c1d23214
Dmitriy <vertyos@mail.ru>
2023-09-29 08:48:44 +0300

    Вывел форму в html, связала форму с моделью

--- weather/views.py
+++ weather/views.py
@@ -1,22 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
+from django.views import View
+
 from .models import City
+from .forms import CityForm
+
 
 # Create your views here.
-def index(request):
-    appid = '11bb77ca1670a956f5ace6631b000e0f'
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=Metric&appid=' + appid
-    cities = City.objects.all()
-    all_cities = []
-    for city in cities:
-        res = requests.get(url.format(city.name)).json()
-        city_info = {
-            'city': city,
-            'temp': res['main']['temp'],
-            'icon': res['weather'][0]['icon']
+class Info(View):
+    def get(self, request):
+        appid = '11bb77ca1670a956f5ace6631b000e0f'
+        url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=Metric&appid=' + appid
+        cities = City.objects.all()
+        all_cities = []
+        for city in cities:
+            res = requests.get(url.format(city.name)).json()
+            city_info = {
+                'city': city,
+                'temp': res['main']['temp'],
+                'icon': res['weather'][0]['icon']
+            }
+            all_cities.append(city_info)
+        context = {
+            'all_info': all_cities,
+            'form': CityForm
         }
-        all_cities.append(city_info)
-    context = {
-        'all_info': all_cities
-    }
-    return render(request, 'weather/index.html', context)
+        return render(request, 'weather/index.html', context)
+
+    def post(self, request):
+        form = CityForm(request.POST)
+        form.save()
+        return redirect('/')
