13cd4329f5d0289a411168be9970274574679e64
Dmitriy <vertyos@mail.ru>
2023-09-29 08:22:22 +0300

    Добавил модель City, что бы в ней хранить название городов

--- weather/views.py
+++ weather/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 import requests
-
+from .models import City
 
 # Create your views here.
 def index(request):
     appid = '11bb77ca1670a956f5ace6631b000e0f'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=Metric&appid=' + appid
-    city = 'London'
-    res = requests.get(url.format(city)).json()
-    city_info = {
-        'city': city,
-        'temp': res['main']['temp'],
-        'icon': res['weather'][0]['icon']
-    }
+    cities = City.objects.all()
+    all_cities = []
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city,
+            'temp': res['main']['temp'],
+            'icon': res['weather'][0]['icon']
+        }
+        all_cities.append(city_info)
     context = {
-        'info': city_info
+        'all_info': all_cities
     }
     return render(request, 'weather/index.html', context)
