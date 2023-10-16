31e65f51f46c6cca41a17724a672a4e102879077
Dmitriy <vertyos@mail.ru>
2023-10-16 07:23:57 +0300

    Добавил возможность удалять города из БД, что бы сокращать список

--- weather/views.py
+++ weather/views.py
@@ -37,33 +37,3 @@ def delit(request, city):
     city.delete()
     return redirect('/')
 
-# class Info(View):
-#
-#     def get(self, request):
-#         appid = '11bb77ca1670a956f5ace6631b000e0f'
-#         url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=Metric&appid=' + appid
-#         cities = City.objects.all()
-#         all = []
-#
-#         async def res(city):
-#             await requests.get(url.format(city.name)).json()
-#
-#
-#         async def main():
-#             tasks = [asyncio.create_task(res(city)) for city in cities]
-#             o = await asyncio.gather(*tasks)
-#             for city in o:
-#                 city_info = {
-#                     'city': city['name'],
-#                     'temp': city['main']['temp'],
-#                     'icon': city['weather'][0]['icon']
-#                 }
-#                 all.append(city_info)
-#         asyncio.run(main())
-#
-#         # @async_to_sync
-#         context = {
-#             'all_info': all,
-#             'form': CityForm
-#         }
-#         return render(request, 'weather/index.html', context)
